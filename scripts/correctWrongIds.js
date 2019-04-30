54fb5fba87b4057936d33011ed3e58e0091b6471
Hugo J. Bello <hugojose@azlo.com>
2019-04-30 19:37:14 +0200

    fix

--- scripts/correctWrongIds.js
+++ scripts/correctWrongIds.js
@@ -20,6 +20,7 @@ const updateDocumentId = (document) => {
             const id = document.scraper_id +"-" + date + "-" + uuidv1();
             document.id = id;
             console.log("---updating----");
+            console.log("new id --> " + id);
             let res = collection.save(document, resolve);
 
         });
@@ -35,7 +36,7 @@ mongoClient.connect(mongoUrl, async function (err, client) {
     const collectionName = "NewsContentScraped";
     const dbName = "testing";
     const collection = client.db(dbName).collection(collectionName);
-    let cursor = collection.find({'id':null});
+    let cursor = collection.find({id: /GMT/i});
     await cursor.each(async function (err, item) {
         console.log(item.date);
         console.log(item.content.substring(0,100));
