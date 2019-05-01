48fd7cc9e2176b2b180986055a4e87eaccbfafba
hugojbello <hjbello.wk@gmail.com>
2019-05-01 17:38:45 +0200

    fix

--- scrapers/ElPaisHistoricScraper.js
+++ scrapers/ElPaisHistoricScraper.js
@@ -4,7 +4,7 @@ const htmlToText = require('html-to-text');
 module.exports = class ElPaisHistoricScraper extends PuppeteerScraper {
     constructor(configPath= "../config/scrapingConfig.json") {
         super(configPath);
-        this.page = 1;
+        this.page = 0;
         this.timeWaitStart = 1 * 1000;
         this.timeWaitClick = 500;
         this.newsCounter = 0;
@@ -12,7 +12,7 @@ module.exports = class ElPaisHistoricScraper extends PuppeteerScraper {
 
     async scrapDate(date) {
         this.date = date;
-        this.page = 1;
+        this.page = 0;
         //https://elpais.com/tag/fecha/20190305/3
         const dateFormated = this.formatDate(date);
         await this.initializePuppeteer();
